af8ca89e50b7ba9c10a1f8288fdfdbf71420b64b
adityaparmar <adityaparmar9815@gmail.com>
2023-10-01 17:34:19 +0530

    1/10/2023

---
+++ reverse_word_in_a_string_3.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int start = 0, end = 0;
+
+        while(end < s.size()) {
+            // Stop when we encounter the end of a word
+            if(s[end] == ' ' || end == s.size()-1) {
+                int ptr = end-1;
+                // In case current word is the final word in the string
+                if(end == s.size()-1) ptr = end;
+
+                // Reverse the word
+                while(start < ptr) swap(s[start++], s[ptr--]);
+                // Start points to the start of the next word
+                start = end+1;
+            }
+            end++;
+        }
+        return s;
+    }
+};
