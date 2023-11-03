f995bca5b3bbbdcb06501a74678344c1216f4fcd
adityaparmar <adityaparmar9815@gmail.com>
2023-11-03 16:57:50 +0530

    3/11/2023

--- build_an_array_with_stack_operations.cpp
+++ build_an_array_with_stack_operations.cpp
@@ -2,16 +2,16 @@ class Solution {
 public:
     vector<string> buildArray(vector<int>& target, int n) {
         vector<string> ans;
-        int curr = 1;
+        int ptr = 0;
 
-        for(int i=0; i < target.size(); i++) {
-            while(i < target.size() && curr != target[i]){
+        for(int i=1; i <= target[target.size()-1]; i++) {
+            if(i == target[ptr]) {
+                ans.push_back("Push");
+                ptr++;
+            } else {
                 ans.push_back("Push");
                 ans.push_back("Pop");
-                curr++;
             }
-            curr++;
-            ans.push_back("Push");
         }
         return ans;
     }
