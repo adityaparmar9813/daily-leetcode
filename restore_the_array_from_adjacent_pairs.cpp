b4fc7c2aba5fac1c7243a37994632afcc3499585
adityaparmar <adityaparmar9815@gmail.com>
2023-11-11 00:23:31 +0530

    10/11/2023

---
+++ restore_the_array_from_adjacent_pairs.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
+        unordered_map<int, vector<int>> dfs;
+        vector<int> result;
+
+        for (auto pair : adjacentPairs) {
+            dfs[pair[0]].push_back(pair[1]);
+            dfs[pair[1]].push_back(pair[0]);
+        }
+
+        for (auto entry : dfs) {
+            if (entry.second.size() == 1) {
+                result = {entry.first, entry.second[0]};
+                break;
+            }
+        }
+
+        while (result.size() < adjacentPairs.size() + 1) {
+            int last = result[result.size() - 1];
+            int prev = result[result.size() - 2];
+            vector<int> candidates = dfs[last];
+
+            int nextElement = (candidates[0] != prev) ? candidates[0] : candidates[1];
+            result.push_back(nextElement);
+        }
+        return result;        
+    }
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
