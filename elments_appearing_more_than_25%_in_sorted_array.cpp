9cf88a291d3c2a5101311b4b4dfc2807c463f4c7
adityaparmar <adityaparmar9815@gmail.com>
2023-12-11 14:26:52 +0530

    11/12/2023

--- elments_appearing_more_than_25%_in_sorted_array.cpp
+++ elments_appearing_more_than_25%_in_sorted_array.cpp
@@ -4,11 +4,10 @@ public:
         int continuous = 1;
 
         for(int i=1; i < arr.size(); i++) {
-            if(arr[i] == arr[i-1]) {
-                continuous++;
-                if(continuous > arr.size()/4) return arr[i];
-            }
+            if(arr[i] == arr[i-1]) continuous++;
             else continuous = 1;
+
+            if(continuous > arr.size() / 4) return arr[i];
         }
         return arr[0];
     }
