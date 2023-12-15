83611ec2fbbd9145cb2b805e41acb9a3427b49ad
adityaparmar <adityaparmar9815@gmail.com>
2023-12-15 23:52:34 +0530

    15/11/2023

--- destination_city.cpp
+++ destination_city.cpp
@@ -7,7 +7,7 @@ public:
         for(auto path: paths) {
             mp[path[0]] = mp[path[0]] + 2;
             mp[path[1]]++;
-        }
+        } 
 
         for(auto &it: mp) if(it.second == 1) return it.first;
         return "";
