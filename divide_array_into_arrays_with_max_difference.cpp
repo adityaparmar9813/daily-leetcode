292fa5c401996def06c1e04c34f4a761247d31f7
adityaparmar <adityaparmar9815@gmail.com>
2024-02-01 12:42:33 +0530

    01/02/2024

---
+++ divide_array_into_arrays_with_max_difference.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> divideArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for(int i=2; i < nums.size(); i=i+3) {
+            if(nums[i]-nums[i-1] > k || nums[i-1]-nums[i-2] > k || nums[i]- nums[i-2] > k) return {};
+            ans.push_back({nums[i-2], nums[i-1], nums[i]});
+        }
+        return ans;
+    }
+};
+
+// Time Complexity: O(nlogn)
+// Space Complexity: O(1)
