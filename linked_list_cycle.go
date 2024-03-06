19e643207c078c06cd15faa02368f9c04f96b563
adityaparmar <adityaparmar9815@gmail.com>
2024-03-06 07:28:23 +0530

    06/03/2024

---
+++ linked_list_cycle.go
@@ -0,0 +1,16 @@
+func hasCycle(head *ListNode) bool {
+    slow, fast := head, head
+
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+
+        if slow == fast {
+            return true
+        }
+    }
+    return false
+}
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
