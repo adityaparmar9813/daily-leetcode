f898a8f718157a59f4a72e69b2886bb7b7010986
adityaparmar <adityaparmar9815@gmail.com>
2024-03-06 07:23:18 +0530

    06/03/2024

---
+++ linked_list_cycle.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast) return true;
+        }
+        return false;
+    }
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(1)
