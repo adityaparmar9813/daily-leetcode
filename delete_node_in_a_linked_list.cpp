434248767ffccd76728331e522b28b83b7635ff7
adityaparmar <adityaparmar9815@gmail.com>
2024-05-05 16:57:55 +0530

    05/05/2024

---
+++ delete_node_in_a_linked_list.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp = node->next;
+        node->val = temp->val;
+        node->next = temp->next;
+        delete temp;
+    }
+};
+
+// Time Complexity: O(1)
+// Space Complexity: O(1)
