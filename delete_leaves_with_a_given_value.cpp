7c6e2fdd587575d858c5f26f5e1bc7cc3a36a11f
adityaparmar <adityaparmar9815@gmail.com>
2024-05-17 05:47:28 +0530

    17/05/2024

---
+++ delete_leaves_with_a_given_value.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    TreeNode* removeLeafNodes(TreeNode* root, int target) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+
+        root->left = removeLeafNodes(root->left, target);
+        root->right = removeLeafNodes(root->right, target);
+
+        if (root->left == nullptr && root->right == nullptr && root->val == target) {
+            return nullptr;
+        }
+        return root;
+    }
+};
+
+// Time Complexity : O(n)
+// Space Complexity : O(n)
