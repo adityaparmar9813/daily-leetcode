d23ae5d9ec5fe8fc6a0ee825992922c38e582876
adityaparmar <adityaparmar9815@gmail.com>
2024-06-01 21:24:15 +0530

    01/06/2024

---
+++ score_of_a_string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int ans = 0;
+
+        for (int i=1; i < s.size(); i++) {
+            ans += abs(s[i] - s[i-1]);
+        }
+        return ans;
+    }
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
