ba92d27c019105e9d7e4f8169985fd85943d695b
adityaparmar <adityaparmar9815@gmail.com>
2024-06-13 16:47:05 +0530

    13/06/2024

---
+++ minimum_number_of_moves_to_seat_everyone.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+        int ans = 0;
+        sort(seats.begin(), seats.end());
+        sort(students.begin(), students.end());
+
+        for (int i = 0; i < seats.size(); i++) {
+            ans += abs(seats[i] - students[i]);
+        }
+        return ans;
+    }
+};
+
+// Time Complexity: O(n*log(n))
+// Space Complexity: O(1)
