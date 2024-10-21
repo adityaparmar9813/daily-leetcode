de46ea227e907b4ebdd0a26d3900fa479986f408
adityaparmar <adityaparmar9815@gmail.com>
2024-10-21 21:33:35 +0530

    20/10/2024

---
+++ parsing_a_boolean_expression.cpp
@@ -0,0 +1,71 @@
+class Solution {
+  public:
+	bool parse_or(vector<int> &res) {
+		int mask = res[0];
+		for (int i = 1; i < res.size(); i++) {
+			mask |= res[i];
+		}
+		return mask;
+	}
+
+	bool parse_and(vector<int> &res) {
+		int mask = res[0];
+		for (int i = 1; i < res.size(); i++) {
+			mask &= res[i];
+		}
+		return mask;
+	}
+
+	bool parseBoolExpr(string s) {
+		int n = s.size();
+		stack<int> st;
+
+		for (int i = 0; i < n; i++) {
+			if (s[i] == ',') {
+				continue;
+			}
+
+			if (s[i] == ')') {
+				vector<int> res;
+
+				while (st.top() != '&' && st.top() != '|' && st.top() != '!') {
+					char ch = st.top();
+					st.pop();
+
+					if (ch == 't') {
+						res.push_back(1);
+					}
+					if (ch == 'f') {
+						res.push_back(0);
+					}
+				}
+
+				char ch = st.top();
+				st.pop();
+
+				if (ch == '&') {
+					if (parse_and(res)) {
+						st.push('t');
+					} else {
+						st.push('f');
+					}
+				} else if (ch == '|') {
+					if (parse_or(res)) {
+						st.push('t');
+					} else {
+						st.push('f');
+					}
+				} else if (ch == '!') {
+					if (res[0] == 0) {
+						st.push('t');
+					} else {
+						st.push('f');
+					}
+				}
+			} else {
+				st.push(s[i]);
+			}
+		}
+		return (st.top() == 't');
+	}
+};
