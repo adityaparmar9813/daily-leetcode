718258c4b4fcfc3d312a65ef26f99a06d0943963
adityaparmar <adityaparmar9815@gmail.com>
2024-10-25 23:31:24 +0530

    25/10/2024

---
+++ remove_sub-folders_from_the_file_system.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+	vector<string> removeSubfolders(vector<string> &folder) {
+		int n = folder.size();
+		sort(folder.begin(), folder.end());
+
+		vector<string> ans = {folder[0]};
+		string prev = folder[0];
+
+		for (int i = 1; i < n; i++) {
+			string s = folder[i];
+
+			if (s.find(prev + '/') != 0) {
+				ans.push_back(s);
+				prev = s;
+			}
+		}
+		return ans;
+	}
+};
+
+// Time Complexity : O(n * log(n))
+// Space Complexity : O(n)
