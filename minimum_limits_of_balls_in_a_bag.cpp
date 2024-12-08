79042ed05b42e50c6439deae33243bebcbe5b888
adityaparmar <adityaparmar9815@gmail.com>
2024-12-08 22:00:31 +0530

    08/12/2024

---
+++ minimum_limits_of_balls_in_a_bag.cpp
@@ -0,0 +1,31 @@
+class Solution {
+  public:
+	bool check(int mid, vector<int> &nums, int maxOperations) {
+		for (int num : nums) {
+			maxOperations -= (num + mid - 1) / mid - 1;
+		}
+
+		return maxOperations >= 0;
+	}
+
+	int minimumSize(vector<int> &nums, int maxOperations) {
+		int n = nums.size();
+		int l = 1, r = 1e9, ans = 1e9;
+
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+
+			if (check(mid, nums, maxOperations)) {
+				ans = mid;
+				r = mid - 1;
+			} else {
+				l = mid + 1;
+			}
+		}
+
+		return ans;
+	}
+};
+
+// Time Complexity: O(n * log(max(nums)))
+// Space Complexity: O(1)
