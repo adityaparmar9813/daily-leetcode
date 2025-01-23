b58a92d1b7e21ec00b27442fb76076460e7182e1
adityaparmar9813 <adityaparmar9815@gmail.com>
2025-01-23 15:38:57 +0530

    23/01/2025

---
+++ count_servers_that_communicate.cpp
@@ -0,0 +1,31 @@
+class Solution {
+   public:
+	int countServers(vector<vector<int>>& grid) {
+		int m = grid.size(), n = grid[0].size();
+		vector<int> row(m, 0), col(n, 0);
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j]) {
+					row[i]++;
+					col[j]++;
+				}
+			}
+		}
+
+		int ans = 0;
+
+		for (int i = 0; i < m; i++) {
+			for (int j = 0; j < n; j++) {
+				if (grid[i][j] && (row[i] > 1 || col[j] > 1)) {
+					ans++;
+				}
+			}
+		}
+
+		return ans;
+	}
+};
+
+// Time Complexity : O(m * n)
+// Space complexity: O(m + n)
