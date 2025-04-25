9dbc4d708f725d942144bff0acde7793705577db
adityaparmar9813 <adityaparmar9815@gmail.com>
2025-04-25 22:49:52 +0530

    25/04/2025

---
+++ count_of_interesting_subarrays.cpp
@@ -0,0 +1,25 @@
+class Solution {
+   public:
+	long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+		long long count = 0, equals = 0;
+		unordered_map<int, long long> mp;
+		mp[0] = 1;
+
+		for (int i : nums) {
+			if (i % modulo == k) {
+				equals++;
+			}
+
+			int rem = equals % modulo;
+			int needed = (rem - k + modulo) % modulo;
+
+			count += mp[needed];
+			mp[rem]++;
+		}
+
+		return count;
+	}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
