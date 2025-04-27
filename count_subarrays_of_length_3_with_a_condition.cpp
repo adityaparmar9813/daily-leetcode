49cc8a75c175f84fb534456c0ec110d9872e72c4
adityaparmar9813 <adityaparmar9815@gmail.com>
2025-04-27 14:28:55 +0530

    26/04/2025

---
+++ count_subarrays_of_length_3_with_a_condition.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+	int countSubarrays(vector<int>& nums) {
+		int ans = 0;
+
+		for (int i = 0; i < nums.size() - 2; i++) {
+			if ((nums[i] + nums[i + 2]) * 2 == nums[i + 1]) {
+				ans++;
+			}
+		}
+
+		return ans;
+	}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
