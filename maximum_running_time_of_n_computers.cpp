9281e88e18efa428d03db3fffbe7c3112eb00357
adityaparmar9813 <adityaparmar9815@gmail.com>
2025-12-01 21:49:19 +0530

    01/12/2025

---
+++ maximum_running_time_of_n_computers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+   public:
+	long long maxRunTime(int n, vector<int>& batteries) {
+		long long totalEnergy = 0;
+		for (int b : batteries) totalEnergy += b;
+
+		sort(batteries.begin(), batteries.end());
+
+		for (int i = batteries.size() - 1; i >= 0; i--) {
+			if (batteries[i] > totalEnergy / n) {
+				totalEnergy -= batteries[i];
+				n--;
+			} else {
+				break;
+			}
+		}
+
+		return totalEnergy / n;
+	}
+};
+
+// Time Complexity: O(n * log(n))
+// Space Complexity: O(1)
