fd0e5f109e546af28dcc4ce45e3f0d9c1da097ab
adityaparmar9813 <adityaparmar9815@gmail.com>
2025-12-20 00:37:27 +0530

    19/12/2025

---
+++ find_all_people_with_secret_repeat.cpp
@@ -0,0 +1,52 @@
+class Solution {
+   public:
+	vector<int> findAllPeople(int n, vector<vector<int>>& meetings,
+	                          int firstPerson) {
+		vector<vector<pair<int, int>>> adj(n);
+		adj[0].push_back({firstPerson, 0});
+		adj[firstPerson].push_back({0, 0});
+
+		for (auto meeting : meetings) {
+			adj[meeting[0]].push_back({meeting[1], meeting[2]});
+			adj[meeting[1]].push_back({meeting[0], meeting[2]});
+		}
+
+		priority_queue<pair<int, int>, vector<pair<int, int>>,
+		               greater<pair<int, int>>>
+		    pq;
+		pq.push({0, 0});
+		vector<int> time(n, INT_MAX);
+		time[0] = 0;
+
+		while (!pq.empty()) {
+			int curTime = pq.top().first;
+			int curr = pq.top().second;
+			pq.pop();
+
+			if (curTime > time[curr]) {
+				continue;
+			}
+
+			for (auto node : adj[curr]) {
+				int v = node.first;
+				int t = node.second;
+
+				if (curTime <= t && t < time[v]) {
+					time[v] = t;
+					pq.push({t, v});
+				}
+			}
+		}
+
+		vector<int> ans;
+		for (int i = 0; i < n; i++) {
+			if (time[i] != INT_MAX) {
+				ans.push_back(i);
+			}
+		}
+		return ans;
+	}
+};
+
+// Time Complexity : O(n ^ 2)
+// Space Complexity : O(n ^ 2)
