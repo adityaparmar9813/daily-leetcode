39c471331a7800966049eed539a4acdc3540c3d6
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-02-02 02:10:27 +0530

    01/02/2026

---
+++ divide_an_array_into_subarrays_with_minimum_cost_i.cpp
@@ -0,0 +1,12 @@
+class Solution {
+   public:
+	int minimumCost(vector<int>& nums) {
+		int first = nums[0];
+		sort(nums.begin() + 1, nums.end());
+
+		return first + nums[1] + nums[2];
+	}
+};
+
+// Time Complexity: O(n * log(n))
+// Space Complexity: O(1)
