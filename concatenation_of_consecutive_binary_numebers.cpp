fe7d7357df5a53ae1ccc937fc25c93879db98cf8
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-02-28 14:30:16 +0530

    28/02/2026

--- concatenation_of_consecutive_binary_numebers.cpp
+++ concatenation_of_consecutive_binary_numebers.cpp
@@ -1,22 +1,20 @@
 class Solution {
    public:
-	const int MOD = 1e9 + 7;
+    int concatenatedBinary(int n) {
+        const int MOD = 1e9 + 7;
+        long long ans = 0;
+        int len = 0;
 
-	int concatenatedBinary(int n) {
-		int mul = 1, ans = 0;
+        for (int i = 1; i <= n; ++i) {
+            if ((i & (i - 1)) == 0) {
+                len++;
+            }
+            
+            ans = ((ans << len) | i) % MOD;
+        }
 
-		for (int i = n; i > 0; i--) {
-			int num = i;
-
-			while (num != 0) {
-				ans = (ans + (num % 2) * mul) % MOD;
-				mul = (mul * 2) % MOD;
-				num /= 2;
-			}
-		}
-
-		return ans;
-	}
+        return ans;
+    }
 };
 
 // Time Complexity : O(n)
