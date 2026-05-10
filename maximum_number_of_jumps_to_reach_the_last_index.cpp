cb5e290c77d41a4167bf4b85121b5fb3763464b8
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-05-11 01:40:53 +0530

    11/05/2026

---
+++ maximum_number_of_jumps_to_reach_the_last_index.cpp
@@ -0,0 +1,21 @@
+class Solution {
+   public:
+	int maximumJumps(vector<int>& nums, int target) {
+		int n = nums.size();
+		vector<int> dp(n, -1e9);
+		dp[0] = 0;
+
+		for (int i = 1; i < n; i++) {
+			for (int j = 0; j < i; j++) {
+				if (abs(nums[j] - nums[i]) <= target) {
+					dp[i] = max(dp[i], dp[j] + 1);
+				}
+			}
+		}
+
+		return dp[n - 1] < 0 ? -1 : dp[n - 1];
+	}
+};
+
+// Time Complexity : O(n ^ 2)
+// Space Complexity : O(n)
