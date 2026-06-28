f0a974d936a4dc400c60ca1c53f5f80721077b1e
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-06-28 23:02:37 +0530

    28/06/2026

---
+++ maximum_element_after_decreasing_and_rearranging_repeat.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+	int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+		sort(arr.begin(), arr.end());
+		arr[0] = 1;
+
+		for (int i = 1; i < arr.size(); i++) {
+			arr[i] = min(arr[i], arr[i - 1] + 1);
+		}
+
+		return arr.back();
+	}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
