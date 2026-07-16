28fdd78a425b8b79f4ee131936de688bf47604fe
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-07-16 21:14:23 +0530

    17/07/2026

---
+++ sum_of_gcd_of_formed_pairs.cpp
@@ -0,0 +1,34 @@
+class Solution {
+   public:
+	long long gcdSum(vector<int>& nums) {
+		int n = nums.size();
+
+		vector<int> mx;
+		int prefixMax = INT_MIN;
+
+		for (int x : nums) {
+			prefixMax = max(prefixMax, x);
+			mx.push_back(prefixMax);
+		}
+
+		vector<int> prefixGcd;
+		for (int i = 0; i < n; ++i) {
+			prefixGcd.push_back(gcd(nums[i], mx[i]));
+		}
+
+		sort(prefixGcd.begin(), prefixGcd.end());
+
+		long long ans = 0;
+		int left = 0, right = n - 1;
+		while (left < right) {
+			ans += gcd(prefixGcd[left], prefixGcd[right]);
+			++left;
+			--right;
+		}
+
+		return ans;
+	}
+};
+
+// Time Complexity: O(n * log(n))
+// Space Complexity: O(n)
