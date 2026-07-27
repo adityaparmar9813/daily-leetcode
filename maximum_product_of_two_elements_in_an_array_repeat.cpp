9e17eedc50f91b6e49c490d2b5ef4ac1fff88f23
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-07-28 01:39:29 +0530

    28/07/2026

---
+++ maximum_product_of_two_elements_in_an_array_repeat.cpp
@@ -0,0 +1,12 @@
+class Solution {
+   public:
+	int maxProduct(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		int n = nums.size();
+		return max((nums[0] - 1) * (nums[1] - 1),
+		           (nums[n - 1] - 1) * (nums[n - 2] - 1));
+	}
+};
+
+// Time Complexity: O(n * log(n))
+// Space Complexity: O(1)
