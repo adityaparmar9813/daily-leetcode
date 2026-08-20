b43114e190b089762af862069761aaf2d152fed9
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-08-20 21:07:32 +0530

    20/08/2026

---
+++ distribute_elements_into_two_arrays_i.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+	vector<int> resultArray(vector<int>& nums) {
+		vector<int> A[2] = {{nums[0]}, {nums[1]}};
+		const int n = nums.size();
+
+		for (int i = 2; i < n; i++) {
+			A[A[0].back() <= A[1].back()].push_back(nums[i]);
+		}
+
+		A[0].insert(A[0].end(), A[1].begin(), A[1].end());
+		return A[0];
+	}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(n)
