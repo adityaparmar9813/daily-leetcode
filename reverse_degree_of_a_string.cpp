ba54e13493011b011910a969a9c1905b12857b6d
adityaparmar9813 <adityaparmar9815@gmail.com>
2026-09-20 13:50:28 +0530

    20/09/2026

---
+++ reverse_degree_of_a_string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+   public:
+	int reverseDegree(string s) {
+		int ans = 0;
+
+		for (int i = 0; i < s.size(); i++) {
+			ans += (26 - (s[i] - 'a')) * (i + 1);
+		}
+
+		return ans;
+	}
+};
+
+// Time Complexity: O(n)
+// Space Complexity: O(1)
